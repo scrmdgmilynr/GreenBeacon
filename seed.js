1ae54af1c25e0043065638736b5f57ad005492b5
Alexander Ting <AlexTing@Alexanders-MacBook-Pro.local>
2016-10-26 10:40:24 -0700

    [add] Updated fellows schema. Seeded fellows database.

--- seed.js
+++ seed.js
@@ -2,3 +2,25 @@ var pg = require('pg');
 var Sequelize = require('sequelize');
 
 var Fellers = require('./db/schema').Fellers;
+
+var fellers = [
+  {
+    fellerName: 'Reed Cureton',
+    githubHandle: 'rcureton00'
+  },
+  {
+    fellerName: 'Blake Contreras',
+    githubHandle: 'blakecontreras'
+  },
+  {
+    fellerName: 'Melba Madrigal',
+    githubHandle: 'melbee'
+  }
+];
+
+fellers.forEach((feller) => {
+  Fellers.create({
+    fellerName: feller.fellerName,
+    githubHandle: feller.githubHandle
+  });
+});
