a1740df95b18a2ad5988ff4dd374dfaa1b5ade0d
ashermanwmf <shermanandrew50@gmail.com>
2016-10-26 10:28:07 -0700

    [add] - working with angular-cookies

--- Client/app/app.js
+++ Client/app/app.js
@@ -1,6 +1,6 @@
 //initialize app module, include services and auth dependencies
 
-angular.module('app', ['app.auth', 'app.queue', 'app.services', 'app.student', 'ngRoute', 'ngSanitize'])
+angular.module('app', ['app.auth', 'app.queue', 'app.services', 'app.student', 'ngRoute', 'ngSanitize', 'ngCookies'])
 
 .config(function($routeProvider){
 
