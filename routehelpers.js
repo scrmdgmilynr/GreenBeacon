30ce078e16630fc3c39c5be38c87c78d0789d568
Jemil Ezzet <jemil.ezzet@gmail.com>
2016-10-27 11:54:53 -0700

    [add] Each chat message has username as well as userId

--- routehelpers.js
+++ routehelpers.js
@@ -6,14 +6,7 @@ var User = require('./db/schema').User;
 var Ticket = require('./db/schema').Ticket;
 var Claim = require('./db/schema').Claim;
 var Feller = require('./db/schema').Feller;
-<<<<<<< HEAD
 var Chat = require('./db/schema').Chat;
-=======
-<<<<<<< HEAD
-=======
-var Chat = require('./db/schema').Chat;
->>>>>>> master
->>>>>>> feature
 
 if(process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
@@ -207,12 +200,21 @@ module.exports = {
       });
   },
 
+  // get ticket's chat
   getChat: function(req, res, next) {
     Chat.findAll({ where: { ticketId: req.body.ticketId } })
       .then(function(chatMessages) {
-        console.log(chatMessages);
-        res.status(200);
-        res.send(chatMessages);
+        Promise.all(chatMessages.map((chatMessage) => {
+          return User.find({ where: { id: chatMessage.dataValues.userId } })
+            .then((user) => {
+              chatMessage.dataValues.username = user.username;
+              return chatMessage;
+            })
+        }))
+          .then((newChatMessages) => {
+            res.status(200);
+            res.send(newChatMessages);
+          })
       });
   },
 
