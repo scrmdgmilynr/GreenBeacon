f465e1a9a9356bf1d64c09145431d96afa10ebaa
Jemil Ezzet <jemil.ezzet@gmail.com>
2016-10-28 15:49:58 -0700

    [fix] Fixed another syntax error

--- Client/app/student/student.js
+++ Client/app/student/student.js
@@ -13,7 +13,7 @@ angular.module('app.student', [])
   });
 
   socket.on('messageAdded', (data) => {
-    document.getElementsById('data').style.display = 'block';
+    document.getElementById('data').style.display = 'block';
     console.log(data);
   });
 
